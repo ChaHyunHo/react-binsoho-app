7e108aaca4b2f3b29e83ea64e133cae6726042f5
chahyeonho <chamym@naver.com>
2021-05-20 21:26:55 +0900

    custom Togglekey settings commit

--- src/CutomToggle.js
+++ src/CutomToggle.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useAccordionToggle } from "react-bootstrap";
+import { AccordionContext } from "react-bootstrap";
 
-export default function CutomToggle({ children, eventKey }) {
-  const decoratedOnClick = useAccordionToggle(eventKey, () =>
-    console.log("totally custom!")
+export default function CutomToggle({ children, eventKey, callback }) {
+  const currentEventKey = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionToggle(
+    eventKey,
+    () => callback && callback(eventKey)
   );
+
+  const isCurrentEventKey = currentEventKey === eventKey;
 
   return (
     <button
       type="button"
-      style={{ backgroundColor: "pink" }}
+      style={{ backgroundColor: isCurrentEventKey ? "pink" : "lavender" }}
       onClick={decoratedOnClick}
     >
       {children}
